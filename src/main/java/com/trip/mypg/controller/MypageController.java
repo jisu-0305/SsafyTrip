f1a11a70c45c7a5a8424fe0f0e5259c559edcb62
이학준 <usad63mbn@gmail.com>
2024-11-19 15:19:38 +0900

    feat: mypage 구조만

---
+++ src/main/java/com/trip/mypg/controller/MypageController.java
@@ -0,0 +1,19 @@
+package com.trip.mypg.controller;
+
+import com.trip.member.dto.RegisterRequestDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("mypg")
+public class MypageController {
+
+}
---
+++ src/main/java/com/trip/mypg/service/MypageServiceImpl.java
@@ -0,0 +1,10 @@
+package com.trip.mypg.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MypageServiceImpl {
+
+}
