ec897303de10ba889e2ea31388274b59f1a7a7b2
DoheeKim <dorosy555@naver.com>
2024-11-20 14:52:14 +0900

    refact : 메서드명 수정

--- src/main/java/com/trip/ai/config/AiConfig.java
+++ src/main/java/com/trip/ai/config/AiConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-class Config {
+class AiConfig {
 
     @Bean
     ChatClient chatClient(ChatClient.Builder builder) {
