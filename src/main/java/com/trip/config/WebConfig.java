a8be8972d5da8a4dd67663b932d2bf34730b5d70
jisu <comjisu0311@naver.com>
2024-11-22 17:48:14 +0900

    config: "CORS 에러 해결용 config 추가"

--- src/main/java/com/trip/config/WebConfig.java
+++ src/main/java/com/trip/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://127.0.0.1:8080") // Swagger UI와 관련된 Origin 허용
+                .allowedOrigins("http://localhost:8080", "http://127.0.0.1:8080", "http://localhost:5173", "http://localhost:5174") // Swagger UI와 관련된 Origin 허용
                 .allowedMethods("GET", "POST", "PATCH", "DELETE") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 인증 정보(쿠키 등) 허용
