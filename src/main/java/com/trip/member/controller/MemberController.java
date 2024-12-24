96ed6a613073df6444501f70433e93808c450287
ucs21 <usad63mbn@gmail.com>
2024-12-24 18:05:54 +0900

    feature: checkSession 추가 #76

--- src/main/java/com/trip/member/controller/MemberController.java
+++ src/main/java/com/trip/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.trip.member.controller;
 
+import com.trip.common.ResponseDto;
 import com.trip.member.dto.LoginRequestDTO;
 import com.trip.member.dto.LoginResponseDTO;
 import com.trip.member.dto.RegisterRequestDTO;
@@ -43,11 +44,11 @@ public class MemberController {
 		return ResponseEntity.ok(isDuplicate);
 	}
 
+
 	@Operation(summary = "로그인", description = "아이디와 비밀번호를 이용하여 로그인 처리")
 	@PostMapping("/login")
 	public ResponseEntity<LoginResponseDTO> login(@Parameter(description = "로그인 시 필요한 회원정보(아이디, 비밀번호).", required = true)
 						@RequestBody LoginRequestDTO loginRequestDTO, HttpSession session) {
-
 		LoginResponseDTO loginResponseDTO = memberService.loginMember(loginRequestDTO);
 
 		session.setAttribute("userId", loginResponseDTO.getUserId());
@@ -59,12 +60,24 @@ public class MemberController {
 
 	}
 
+
 	@Operation(summary = "로그아웃", description = "세션 정보를 이용하여 로그아웃")
 	@PostMapping("/logout")
-	private ResponseEntity<String> logout(HttpSession session) {
+	private ResponseEntity<ResponseDto> logout(HttpSession session) {
 
 		session.invalidate();
-		return ResponseEntity.ok("로그아웃이 완료되었습니다.");
+		return ResponseEntity.ok(ResponseDto.success("로그아웃 완료!"));
 	}
 
+
+	@GetMapping("/check-session")
+	private ResponseEntity<ResponseDto> checkSession(HttpSession session){
+		Object userId = session.getAttribute("userId");
+
+		if(userId != null) {
+			return ResponseEntity.ok(ResponseDto.success("Session 체크 완료!"));
+		}else{
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ResponseDto.failure("Session에 해당 User 정보가 없음"));
+		}
+	}
 }
