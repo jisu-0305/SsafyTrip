b0e87c3bfa4451cf48cf28d9a8e819fec47ea2cc
김지수 <comjisu0311@naver.com>
2024-12-26 23:49:36 +0900

    feat: "Swagger 관련 인터셉터 적용" #83

--- src/main/java/com/trip/config/WebConfig.java
+++ src/main/java/com/trip/config/WebConfig.java
@@ -1,13 +1,18 @@
 package com.trip.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final SwaggerInterceptor swaggerInterceptor;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -22,5 +27,11 @@ public class WebConfig implements WebMvcConfigurer {
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurer.addPathPrefix("/api",
                 c -> c.isAnnotationPresent(RestController.class));
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(swaggerInterceptor)
+                .addPathPatterns("/api/**");
     }
 }
