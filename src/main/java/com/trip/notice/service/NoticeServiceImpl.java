a7673f99e92e09977e5acac715b1df24e217f835
ucs21 <usad63mbn@gmail.com>
2025-01-26 00:45:32 +0900

    refactor: NoticeService Image업로드 API 추가 #111

--- src/main/java/com/trip/notice/service/NoticeServiceImpl.java
+++ src/main/java/com/trip/notice/service/NoticeServiceImpl.java
@@ -2,10 +2,15 @@ package com.trip.notice.service;
 
 import com.trip.notice.dto.*;
 import com.trip.notice.mapper.NoticeMapper;
+import com.trip.review.dto.S3ResponseDTO;
+import com.trip.review.util.S3Util;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,6 +21,8 @@ public class NoticeServiceImpl implements NoticeService {
 
     private final NoticeMapper noticeMapper;
     private static final int MIN_PAGE = 1;
+    private final S3Util s3Util;
+    private final String FOLDER_NAME  = "notice";
 
     @Override
     public PagedNoticeResponseDto getPagedNotices(int page, int size, String keyword) {
@@ -65,5 +72,30 @@ public class NoticeServiceImpl implements NoticeService {
         getNoticeById(noticeId);
         noticeMapper.deleteNotice(noticeId);
         return NoticeDeleteResponseDto.of();
+    }
+
+    // s3를 통해 이미지 업로드
+    public S3ResponseDTO uploadImage(MultipartFile request) {
+
+        S3ResponseDTO s3ResponseDTO= null;
+        try {
+            s3ResponseDTO =s3Util.imageUpload(request, FOLDER_NAME);
+        } catch (IOException e) {
+
+        }
+
+        return s3ResponseDTO;
+    }
+
+    // s3에 저장된 이미지 삭제하기
+    public void deleteImage(HttpSession session){
+
+        List<String> s3KeyList = (List<String>) session.getAttribute("S3Keys");
+        if (s3KeyList != null) {
+            s3KeyList.parallelStream().forEach(key -> s3Util.deleteImage(key, FOLDER_NAME));
+        }
+
+        session.removeAttribute("S3Keys");
+        session.removeAttribute("S3Urls");
     }
 }
